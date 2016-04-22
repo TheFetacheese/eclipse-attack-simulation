70dfa9cee7f06bb3c1d46708398a61980aea4352
Alexander Moy <alexandersmoy@gmail.com>
2016-04-22 10:26:43 -0400

    Update EclipseAttack.java

--- EclipseAttack.java
+++ EclipseAttack.java
@@ -1,4 +1,15 @@
+import java.util.Random;
+public class EclipseAttack{
+public static int blockchain = 0; //"main blockchain": this might not make sense since there will be multiple at once
 
-public class EclipseAttack {
+
+/*create a new miner
+ * 
+ */
+public Miner newMiner() {
+	Miner newMiner = new Miner(1);
+	return newMiner;
+}
+
 
 }
