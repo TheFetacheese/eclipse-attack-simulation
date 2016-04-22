e7e922ec617f886c1e58b5268404d0d4d686a3f1
Alexander Moy <alexandersmoy@gmail.com>
2016-04-22 09:26:44 -0400

    class for each miner

---
+++ Miner.java
@@ -0,0 +1,18 @@
+
+public class Miner {
+
+	/* Begin attempting to discover a block on the chain. Each miner
+	 *  is constantly guessing a random number. When it hits the correct
+	 *  number, it is rewarded with a block.
+	 */
+	public void mineBlocks() {
+		
+	}
+	
+	/*not sure how I want this to work yet
+	 * 
+	 */
+	public void findConnections() {
+		
+	}
+}
