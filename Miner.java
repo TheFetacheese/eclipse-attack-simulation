021f8018fb74ae55d3f81b2ccb1bc8f1e091ba11
Alexander Moy <alexandersmoy@gmail.com>
2016-05-03 20:43:02 -0400

    Update Miner.java

--- Miner.java
+++ Miner.java
@@ -3,8 +3,8 @@ public class Miner {
 	public int ownBlockchain = 0;	//amount of blocks known that have not been added
 	public int blocksWon = 0;	//amount of blocks that have been added to the main chain. This might get fucky with eclipse attacks
 	public int miningPower = 0;
-
-	private long[] connections = new long[6]; //arbitrarily set to have each miner have at most 6 connections. IDs of other Miners will be stored in here.
+	
+	private long[] connections = new long[8]; //arbitrarily set to have each miner have at most 6 connections. IDs of other Miners will be stored in here.
 	private int connectionCount = 0;
 	private final long ID;
 	/* Begin attempting to discover a block on the chain. Each miner
@@ -23,11 +23,18 @@ public class Miner {
 		
 	}
 	
+	public boolean hasAllConnections(){
+		if(true){ //TODO: all connections must be filled
+			return true;
+		}
+		return false;
+	}
+	
 	/* not sure how I want this to work yet
 	 * 
 	 */
 	public void findConnections(Queue pool) {
-            if(connectionCount < 6 && pool.peek() != null && pool.peek() != this) {
+            if(connectionCount < 8 && pool.peek() != null && pool.peek() != this) {
                 Miner m = pool.peek();
                 //search for this Miner node in our present connections
                 boolean newNode = true;
