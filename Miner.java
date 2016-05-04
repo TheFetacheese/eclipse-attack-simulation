03c55df2cb1e51fbb239e1b06b5861d3a6479aed
ssauvageau- <ceno1235@yahoo.com>
2016-05-04 15:41:33 -0400

    Formatted and added code for connection selection

--- Miner.java
+++ Miner.java
@@ -4,11 +4,17 @@ public class Miner {
 	public int ownBlockchain = 0;	//amount of blocks known that have not been added
 	public int blocksWon = 0;	//amount of blocks that have been added to the main chain. This might get fucky with eclipse attacks
 	public int miningPower = 0;
+	
+	public final double networkNoise = 0.0001;
   
   	private Tuple[][] triedTable = new Tuple[64][64];
+  	private int triedCount = 0;
   	private Tuple[][] newTable = new Tuple[256][64];
+  	private int newCount = 0;
+  	
+  	private boolean isTrash;
 	
-	private long[] connections = new long[8]; 
+	private Miner[] connections = new Miner[8]; 
 	private int connectionCount = 0;
 	private final long ID;
   	private String ipAddress;
@@ -20,99 +26,111 @@ public class Miner {
 	 */
 	public Miner(int miningPower, String IP, Random r){ //TODO: add a unique ID (IP address?)
 		this.miningPower = miningPower;
-      	this.ipAddress = IP;
-      	this.rand = r;
+      		this.ipAddress = IP;
+      		this.rand = r;
 		this.ID = Math.abs((System.currentTimeMillis() ^ 0xffffffffffffffffL) & (long)~miningPower); //minor bitwise hashing; should be sufficient at preventing collisions with a respectable sample size
+		this.isTrash = false;
+	}
+	
+	public Miner(int miningPower, String IP, Random r, boolean b)
+	{
+		this.miningPower = miningPower;
+      		this.ipAddress = IP;
+      		this.rand = r;
+		this.ID = Math.abs((System.currentTimeMillis() ^ 0xffffffffffffffffL) & (long)~miningPower); //minor bitwise hashing; should be sufficient at preventing collisions with a respectable sample size
+		this.isTrash = b;
 	}
   
   	public String get16Prefix()
-    {
-    	return this.ipAddress.split(".")[0]+this.ipAddress.split(".")[1];  
-    }
+    	{
+    		return this.ipAddress.split(".")[0]+this.ipAddress.split(".")[1];  
+    	}
   
   	public void addToNewTable(Miner addr, Miner src)
-    {
-    	long l = (addr.getID() ^ (~Long.parseLong(addr.get16Prefix(), 36) / Long.parseLong(src.get16Prefix(), 36))) % 32;
-      	int Bucket = new Long(addr.getID() * Long.parseLong(addr.get16Prefix(), 36) * l).hashCode() % 256;
-      	boolean check = false;
-      	for(int i = 0; i < newTable[Bucket].length; i++)
-        {
-        	if(newTable[Bucket][i] == null)
-            {
-            	newTable[Bucket][i] = new Tuple(System.currentTimeMillis(), addr);
-              	check = true;
-              	break;
-            }
-        }
-      	if(check)
-        	return;
-      	else //isTerrible + eviction
-        {
-          	boolean check2 = false;
-        	for(int i = 0; i < newTable[Bucket].length; i++)
-            {
-            	if(System.currentTimeMillis() - newTable[Bucket][i].getTimestamp() > 60000) //if this address has been sitting in here for A WHOLE MINUTE...
-                {
-                	newTable[Bucket][i] = new Tuple(System.currentTimeMillis(), addr);
-                  	check2 = true;
-                  	break;
-                }
-            }
-          	if(check2)
-              	return;
-          	else
-            {
-            	Tuple temp = new Tuple(0, null);
-          		int tmp = 0;
-          		for(int i = 0; i < 4; i++)
-            	{
-             		int j = rand.nextInt(64);
-              		Tuple t = newTable[Bucket][j];
-              		if(t.getTimestamp() > temp.getTimestamp())
-                	{
-                  		temp = t;
-                  		tmp = j;
-                	}
-            	}
-          		newTable[Bucket][tmp] = new Tuple(System.currentTimeMillis(), addr);
-            }
-        }
-    }
+    	{
+    		long l = (addr.getID() ^ (~Long.parseLong(addr.get16Prefix(), 36) / Long.parseLong(src.get16Prefix(), 36))) % 32;
+      		int Bucket = new Long(addr.getID() * Long.parseLong(addr.get16Prefix(), 36) * l).hashCode() % 256;
+      		boolean check = false;
+      		for(int i = 0; i < newTable[Bucket].length; i++)
+        	{
+        		if(newTable[Bucket][i] == null)
+            		{
+            			newTable[Bucket][i] = new Tuple(System.currentTimeMillis(), addr);
+              			check = true;
+              			newCount++;
+              			break;
+            		}
+        	}
+      		if(check)
+        		return;
+      		else //isTerrible + eviction
+        	{
+          		boolean check2 = false;
+        		for(int i = 0; i < newTable[Bucket].length; i++)
+            		{
+            			if(System.currentTimeMillis() - newTable[Bucket][i].getTimestamp() > 60000) //if this address has been sitting in here for A WHOLE MINUTE...
+                		{
+                			newTable[Bucket][i] = new Tuple(System.currentTimeMillis(), addr);
+                  			check2 = true;
+                  			break;
+        			}
+            		}
+          		if(check2)
+              			return;
+          		else
+            		{
+            			Tuple temp = new Tuple(0, null);
+          			int tmp = 0;
+          			for(int i = 0; i < 4; i++)
+            			{
+             				int j = rand.nextInt(64);
+              				Tuple t = newTable[Bucket][j];
+              				if(t.getTimestamp() > temp.getTimestamp())
+                			{
+                  				temp = t;
+                  				tmp = j;
+                			}
+            			}
+          			newTable[Bucket][tmp] = new Tuple(System.currentTimeMillis(), addr);
+            		}
+        	}
+    	}
   
   	public void addToTriedTable(Miner m)
-    {
-    	long l = m.getID() ^ Long.parseLong(m.getIPAddress(), 36) % 4;
-      	int Bucket = new Long((m.getID() * Long.parseLong(m.get16Prefix(), 36) * l)).hashCode() % 64;
-      	boolean check = false;
-      	for(int i = 0; i < triedTable[Bucket].length; i++)
-        {
-        	if(triedTable[Bucket][i] == null)
-            {
-              	triedTable[Bucket][i] = new Tuple(System.currentTimeMillis(), m);
-              	check = true;
-              	break;
-            }
-        }
-      	if(check)
-        	return;
-      	else //bitcoin eviction
-        {
-          	Tuple temp = new Tuple(0, null);
-          	int tmp = 0;
-          	for(int i = 0; i < 4; i++)
-            {
-             	int j = rand.nextInt(64);
-              	Tuple t = triedTable[Bucket][j];
-              	if(t.getTimestamp() > temp.getTimestamp())
-                {
-                  	temp = t;
-                  	tmp = j;
-                }
-            }
-          	this.addToNewTable(temp.getMiner(), this);
-          	triedTable[Bucket][tmp] = new Tuple(System.currentTimeMillis(), m);
-        }
-    }
+    	{
+    		long l = m.getID() ^ Long.parseLong(m.getIPAddress(), 36) % 4;
+      		int Bucket = new Long((m.getID() * Long.parseLong(m.get16Prefix(), 36) * l)).hashCode() % 64;
+      		boolean check = false;
+      		for(int i = 0; i < triedTable[Bucket].length; i++)
+        	{
+        		if(triedTable[Bucket][i] == null)
+            		{
+              			triedTable[Bucket][i] = new Tuple(System.currentTimeMillis(), m);
+              			check = true;
+              			triedCount++;
+              			break;
+            		}
+        	}
+      		if(check)
+        		return;
+      		else //bitcoin eviction
+        	{
+          		Tuple temp = new Tuple(0, null);
+          		int tmp = 0;
+          		for(int i = 0; i < 4; i++)
+            		{
+             			int j = rand.nextInt(64);
+              			Tuple t = triedTable[Bucket][j];
+              			if(t.getTimestamp() > temp.getTimestamp())
+                		{
+                  			temp = t;
+                  			tmp = j;
+                		}
+            		}
+          		this.addToNewTable(temp.getMiner(), this);
+          		triedTable[Bucket][tmp] = new Tuple(System.currentTimeMillis(), m);
+        	}
+    	}
   
 	public void mineBlocks(int goal) {
 		
@@ -125,22 +143,120 @@ public class Miner {
 		return false;
 	}
 	
-	/* not sure how I want this to work yet
-	 * 
-	 */
-	public void findConnections() {
-            
-	}
+	public void findConnections()
+	{
+        	if(this.hasAllConnections())
+        		return;
+		findConnectionsHelper(0);
+        }
         
-    /*
-    * Another miner has found this miner on the pool Queue; establish a connection to him/her!
-    */
-    private void addConnection(Miner m)
-    {
-    	this.connections[this.connectionCount] = m.getID();
-        this.connectionCount++;
-    }
-	/* announce all found blocks to connections
+        public void findConnectionsHelper(int r)
+        {
+        	int rejections = r;
+		double ratio = Math.sqrt((double)triedCount/(double)newCount);
+            	boolean useTried = rand.nextDouble() < ((ratio*(9-connectionCount))/((connectionCount + 1) + (ratio * (9-connectionCount))));
+            	if(useTried)
+            	{
+            		while(true)
+            		{
+            			int randBucket = rand.nextInt(64);
+            			boolean canUse = false;
+            			for(int i = 0; i < triedTable[randBucket].length; i++)
+            			{
+            				if(triedTable[randBucket][i] != null)
+            				{
+            					canUse = true;
+            					break;
+            				}
+            			}
+            			if(canUse)
+            			{
+            				int pos = rand.nextInt(64);
+            				if(triedTable[randBucket][pos] != null)
+            				{
+            					double difference = (System.currentTimeMillis() - triedTable[randBucket][pos].getTimestamp())/1000L;
+            					boolean reject = rand.nextDouble() > Math.min(1, Math.pow(1.2, rejections)/(1+difference));
+            					if(reject)
+            					{
+            						findConnectionsHelper(rejections++);
+            						return;
+            					}
+            					else
+            					{
+            						boolean connect = establishConnection(triedTable[randBucket][pos].getMiner());
+            						if(!connect)
+            						{
+            							findConnectionsHelper(rejections);
+            						}
+            						else
+            							return;
+            					}
+            				}
+            			}
+            		}
+            	}
+            	else //use new table; main difference being that new table has 256 buckets and if we successfully connect we add the entry to the tried table, removing it from the new table
+            	{
+            		while(true)
+            		{
+            			int randBucket = rand.nextInt(256);
+            			boolean canUse = false;
+            			for(int i = 0; i < newTable[randBucket].length; i++)
+            			{
+            				if(newTable[randBucket][i] != null)
+            				{
+            					canUse = true;
+            					break;
+            				}
+            			}
+            			if(canUse)
+            			{
+            				int pos = rand.nextInt(64);
+            				if(newTable[randBucket][pos] != null)
+            				{
+            					double difference = (System.currentTimeMillis() - newTable[randBucket][pos].getTimestamp())/1000L;
+            					boolean reject = rand.nextDouble() > Math.min(1, Math.pow(1.2, rejections)/(1+difference));
+            					if(reject)
+            					{
+            						findConnectionsHelper(rejections++);
+            						return;
+            					}
+            					else
+            					{
+            						boolean connect = establishConnection(newTable[randBucket][pos].getMiner());
+            						if(!connect)
+            						{
+            							findConnectionsHelper(rejections);
+            						}
+            						else
+            						{
+            							Tuple t = newTable[randBucket][pos];
+            							newTable[randBucket][pos] = null;
+            							newCount--;
+            							this.addToTriedTable(t.getMiner());
+            							return;
+            						}
+            					}
+            				}
+            			}
+            		}
+            	}
+        }
+        
+        public boolean establishConnection(Miner m)
+        {
+        	if(m.isTrash() || rand.nextDouble < networkNoise) //1:10000 chance of failing due to network error
+        		return false;
+        	this.connections[connectionCount++] = m;
+        	return true;
+        }
+        
+        public boolean isTrash()
+        {
+        	return this.isTrash;
+        }
+        
+    	/* announce all found blocks to connections
 	 * 
 	 */
 	public void announceBlocks(){
