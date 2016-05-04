46210aa13fa9333bd70f0aee8f23e8e088659ba8
ssauvageau- <ceno1235@yahoo.com>
2016-05-03 21:36:37 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,21 +4,13 @@ public class Main {
     static Random rand = new Random(123456);
     public static void main(String[] args){
         Miner[] miners = new Miner[45];
-        Queue<Miner> connectionQueue = new Queue<>();
         for(int i = 0; i < miners.length; i++)
-            miners[i] = new Miner(rand.nextInt(500));
-        int j = 0;
-        //this presently runs forever. I'm working on that.
-        for(int i = 0; i < miners.length;)
         {
-            connectionQueue.enqueue(miners[i]);
-            if(j%4 == 0 && i < miners.length - 1)
-            {
-                connectionQueue.enqueue(miners[++i]);
-            }
-            j++;
-            System.out.println(i);
-            miners[i].findConnections(connectionQueue);
+            miners[i] = new Miner(rand.nextInt(500), rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256), rand);
+    	    for(int j = 0; j < i; j++)
+       		{
+            
+          	}
         }
     }
 }
