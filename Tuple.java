ba90e375db38b6761fe7b9938e66f6f7898dd798
ssauvageau- <ceno1235@yahoo.com>
2016-05-03 21:37:47 -0400

    Create Tuple.java

---
+++ Tuple.java
@@ -0,0 +1,18 @@
+public class Tuple {
+ 	private long timestamp;
+  	private Miner min;
+  
+  	public Tuple(long l, Miner m)
+    {
+    	this.timestamp = l;
+      	this.min = m;
+    }
+  	public long getTimestamp()
+    {
+    	return this.timestamp;  
+    }
+  	public Miner getMiner()
+    {
+     	return this.min; 
+    }
+}
