03493b726d48f1f8a8caa1cf7ab27bc71685cfbe
Alexander Moy <alexandersmoy@gmail.com>
2016-05-05 23:44:31 -0400

    Update AttackerNode.java

--- AttackerNode.java
+++ AttackerNode.java
@@ -1,9 +1,13 @@
+import java.util.Random;
 
 public class AttackerNode extends Miner{
 
-	public AttackerNode(int miningPower) {
-		super(miningPower);
+	public AttackerNode(int miningPower, String IP, Random R) {
+		super(miningPower, IP, R);
 		// TODO Auto-generated constructor stub
 	}
 
+	public void mineBlocks(int goal){
+		//literally do nothing
+	}
 }
