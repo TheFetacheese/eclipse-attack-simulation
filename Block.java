3922f28331f09d7b19ebb9268a088c01e20e1d3f
Alexander Moy <alexandersmoy@gmail.com>
2016-05-05 20:12:27 -0400

    Update Block.java

--- Block.java
+++ Block.java
@@ -3,16 +3,24 @@ public class Block {
 
 	public int nonce;
 	public int winner;
+	public Block next;
 	
-	public BlockchainNode(int n, int w){
+	public Block(int n, int w){
+		next = null;
 		nonce = n;
 		winner = w;
 	}
 	
+	public Block getNext(){
+		return next;
+	}
 	public int getNonce(){
 		return nonce;
 	}
-	
+	public void setNext(Block next){
+		this.next = next;
+		
+	}
 	public int getWinner(){
 		return winner;
 	}
