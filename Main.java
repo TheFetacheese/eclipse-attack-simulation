7b032b1707934467349fe1032865057da4f02004
Alexander Moy <alexandersmoy@gmail.com>
2016-05-05 23:42:44 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,37 @@ import java.util.Random;
 public class Main {
     static Random rand = new Random(123456);
     public static void main(String[] args){
-        Miner[] miners = new Miner[45];
-        for(int i = 0; i < miners.length; i++)
-        {
-            miners[i] = new Miner(rand.nextInt(500), rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256), rand);
-    	    for(int j = 0; j < i; j++)
+    	int temprand;
+    	int i;
+    	long start;
+    	long stop;
+        Miner[] miners = new Miner[1050];
+        for(i = 0; i < 150; i++)
+        {	
+            miners[i] = new Miner(1, rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256), rand);
+        }
+        for(i = 150; i < 1050; i++){
+        	miners[i] = new AttackerNode(1, rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256)+"."+rand.nextInt(256), rand);
+        }
+        for(i = 0; i < miners.length; i++){
+        	start = System.currentTimeMillis();
+        	stop = start + 3000; //some poor nodes don't get 8 friends, this silences their hopes and dreams
+        	while (miners[i].hasAllConnections() == false){
+        		if (System.currentTimeMillis() < stop) {
+        		temprand = rand.nextInt(miners.length);
+        	if (temprand != i)
+        		miners[i].connect(miners[temprand]);
+        		}
+        		else
+        			break;
+        	}
+        	
+        }
+        for (int q=0; q < 10000; q++){
+            for(int j = 0; j < i; j++)
        		{
-            
+            miners[j].mineBlocks(rand.nextInt(50));
           	}
         }
     }
-}
+    }
